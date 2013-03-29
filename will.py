e7c1633c7290c6665424624c6b40231cbce98782
Will Gwozdz <wgwozdz@princeton.edu>
2013-03-29 00:19:56 -0400

    Edited will.py

--- will.py
+++ will.py
@@ -1,2 +1,35 @@
+from bs4 import BeautifulSoup
+import urllib
+import urllib2
+import re
+
+
 def getData():
-    return ['will', 'test', 'data']
+    data = parsePage("http://facilities.princeton.edu/dining/_Foodpro/menuSamp.asp?locationNum=01&locationName=Rockefeller+%26+Mathey+Colleges&sName=Princeton+University+Dining+Services&naFlag=1")
+    #return ['will', 'test', 'data']
+    return data
+
+
+def parsePage(page):
+    ref = urllib2.urlopen(page)
+    webpage = ref.read()
+    soup = BeautifulSoup(webpage)
+    pt = soup.get_text()
+    pattern = "Lunch"
+    m = re.search(pattern, pt)
+    index = -1
+    fn=[]
+    fn.append([])
+    fn.append([])
+    fn.append([])
+    names =  soup.find_all('a')
+    for s in names:
+        if s['name'] == "Breakfast" or s['name'] == "Lunch" or s['name'] == "Dinner":
+            index += 1
+        else:
+            fn[index].append(s.text)
+    for s in fn[0]:
+        print s
+    return ""
+
+getData()
