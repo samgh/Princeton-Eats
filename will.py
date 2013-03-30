c563d87a1eeab759ae937108c33ddb4a72be8545
Will Gwozdz <wgwozdz@princeton.edu>
2013-03-30 16:39:28 -0400

    Cleaned up parsing and subingredients

--- will.py
+++ will.py
@@ -1,32 +1,44 @@
-import bs4
 from bs4 import BeautifulSoup
 import urllib
 import urllib2
 import re
+import string
 
 class Entree:
     name = ""
     ingredients = []
     allergens = []
     def string(self):
-        s = "Name:"+self.name + '\n'
-        s += "Ingredients:"
-        for i in self.ingredients:
-            s += (i+",")
-        s += "\nAllergens:"
-        for a in self.allergens:
-            s += (a+",")
+        s = "Name:        "+self.name + '\n'
+        s += "Ingredients: "
+        for i in range(0, len(self.ingredients)):
+            if i == len(self.ingredients)-1:
+                s += self.ingredients[i]
+            else:
+                s += (self.ingredients[i]+", ")
+        s += "\nAllergens:   "
+        for i in range(0, len(self.allergens)):
+            if i == len(self.allergens)-1:
+                s += self.allergens[i]
+            else:
+                s += (self.allergens[i]+", ")
         s += "\n"
         return s
 
     def html_string(self):
-        s = "Name:"+self.name + "<br>"
-        s += "Ingredients:"
-        for i in self.ingredients:
-            s += (i+",")
-        s += "<br>Allergens:"
-        for a in self.allergens:
-            s += (a+",")
+        s = "Name:        "+self.name + "<br>"
+        s += "Ingredients: "
+        for i in range(0, len(self.ingredients)):
+            if i == len(self.ingredients)-1:
+                s += self.ingredients[i]
+            else:
+                s += (self.ingredients[i]+", ")
+        s += "<br>Allergens:   "
+        for i in range(0, len(self.allergens)):
+            if i == len(self.allergens)-1:
+                s += self.allergens[i]
+            else:
+                s += (self.allergens[i]+", ")
         s += "<br><br>"
         return s
 
@@ -38,14 +50,20 @@ class Menu:
     def string(self):
         s = ""
         s += "Breakfast:\n"
+        s += "----------------------------------------------------------\n"
         for e in self.breakfast:
-            s += e.html_string()
-            s += "Lunch:\n"
+            s += e.string()
+            s += "----------------------------------------------------------\n"
+        s += "\nLunch:\n"
+        s += "----------------------------------------------------------\n"
         for e in self.lunch:
-            s += e.html_string()
-            s += "Dinner:\n"
+            s += e.string()
+            s += "----------------------------------------------------------\n"
+        s += "\nDinner:\n"
+        s += "----------------------------------------------------------\n"
         for e in self.dinner:
-            s += e.html_string()
+            s += e.string()
+            s += "----------------------------------------------------------\n"
         return s
     def html_string(self):
         s = ""
@@ -58,13 +76,13 @@ class Menu:
         s += "<h3>Dinner:</h3>"
         for e in self.dinner:
             s += e.html_string()
+        # May not be necessary, not liked by ascii
         s.replace(u'\xa0', ' ').encode('utf-8')
         return s
 
 def getData():
     root = "http://facilities.princeton.edu/dining/_Foodpro/"
     data = parseMenuPage(root, "http://facilities.princeton.edu/dining/_Foodpro/menuSamp.asp?locationNum=01&locationName=Rockefeller+%26+Mathey+Colleges&sName=Princeton+University+Dining+Services&naFlag=1")
-    #return ['will', 'test', 'data']
     return data
 
 
@@ -91,6 +109,8 @@ def parseMealPage(root, page):
     entrees = soup.find_all('a')
     ents = []
     for s in entrees[1:]:
+        if s.text == "Top of Page":
+            continue
         e = Entree()
         e.name = s.text
         parse = parseEntreePage(root,root+ s['href'])
@@ -111,10 +131,45 @@ def parseEntreePage(root, page):
     lines = text.split('\n')
     for s in lines:
         if s[:12] == "INGREDIENTS:":
-            ingred = (s[13:].split(','))
+            str = s[13:]
+            str = string.replace(str, " (",", ")
+            str = string.replace(str, ")", "")
+            ingred = (str.split(','))
+            # Some foods may not have ingredients, dont want a crash
+            if isEmpty(ingred[0]):
+                ingred = []
+                continue
+            for i in range(0, len(ingred)):
+                ingred[i] = stripSpace(ingred[i])
         if s[:10] == "ALLERGENS:":
             allerg = (s[10:].split(','))
+            # Some foods don't have allergens, causes problems
+            if isEmpty(allerg[0]):
+                allerg = []
+                continue
+            for i in range(0, len(allerg)):
+                allerg[i] = stripSpace(allerg[i])
     #vals = soup.find_all('span')
     #print vals
     return [ingred, allerg]
 
+def isEmpty(str):
+    str = str.replace(u'\xa0', ' ').encode('utf-8')
+    for i in range(0, len(str)):
+        if str[i] != " ":
+            return False
+    return True
+
+def stripSpace(str):
+    str = str.replace(u'\xa0', ' ').encode('utf-8')
+    findex = 0
+    bindex = len(str)
+    if bindex == 0:
+        return ""
+    while str[findex] == " ":
+        findex += 1
+    while str[bindex-1] == " ":
+        bindex -= 1
+    return str[findex:bindex]
+
+print getData().string()
