47d3725076806c33865b71eb3cf083a29e93d471
dlackey <davidmlackey@me.com>
2013-05-10 23:02:37 -0400

    added 404 page

--- main.py
+++ main.py
@@ -153,3 +153,10 @@ app = webapp2.WSGIApplication([
     ('/timeline', Timeline),
     ('/', Home)
 ], debug=True)
+
+def handle_404(request, response, exception):
+    template = jinja.get_template('templates/404.html')
+    response.out.write(template.render({}))
+    response.set_status(404)
+
+app.error_handlers[404] = handle_404
